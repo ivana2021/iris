deba21097de356e0a4da488a4a1a3b57a6f8c4bb
ivana2021 <85759279+ivana2021@users.noreply.github.com>
2024-01-25 00:16:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1qEwt249KEPH5uYbyt9oKs9U_Ulgp0gEz
 """
 
-!pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import numpy as np
